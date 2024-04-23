8340304b34de8ee8dfba42468b9b4f2d13782787
Raul Fernandes <63769448+RaulTheMiner17@users.noreply.github.com>
2024-04-23 18:21:32 +0530

    Add files via upload

---
+++ Inher/Inher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inher
+{
+    public class Person
+    {
+        public string name;
+        public Person(string n)
+        {
+            name = n;
+        }
+        public void GetName()
+        {
+            Console.WriteLine(name);
+        }
+        static void Main(string[] args)
+        {
+            Person p1 = new Person(" raul ");
+            p1.GetName();
+        }
+    }
+}
