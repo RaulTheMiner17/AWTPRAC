c4b59faf4fc2c53b98b02ac53bf1d1f4b19189a4
Raul Fernandes <63769448+RaulTheMiner17@users.noreply.github.com>
2024-04-23 18:16:59 +0530

    Add files via upload

---
+++ Factorial/Factorial/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactorialLibrary
+{
+    public static class Factorial
+    {
+        public static long Calculate(long n)
+        {
+
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            long result = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                {
+                    result = result*i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Factorial/FactorialTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FactorialLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter No: ");
+        long n = Convert.ToInt32(Console.ReadLine());
+        long result = Factorial.Calculate(n);
+        Console.WriteLine("Factorial of " + n + " is: " + result);
+    }
+}
