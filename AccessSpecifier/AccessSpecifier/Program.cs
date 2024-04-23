80b08d5ea1e5ee07938b37ea2ac3a296ac295155
Raul Fernandes <63769448+RaulTheMiner17@users.noreply.github.com>
2024-04-23 18:12:50 +0530

    Add files via upload

---
+++ AccessSpecifier/AccessSpecifier/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifier
+{
+    public class Person
+    {
+        internal string name;
+        public int age;
+        public Person(string n, int a)
+        {
+            name= n;
+            age= a;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+    }
+    class Student:Person
+    {
+        public int rollno;
+        public Student(string n, int a, int r):base(n, a)
+        { 
+            rollno = r;
+            name = n;
+        }
+    }
+    class TestDemo
+    {   
+        static void Main(string[] args)
+        {
+           
+            Student s1 = new Student("Joshi", 69, 65);
+            Console.WriteLine(s1.name);
+            Console.WriteLine(s1.age);
+            Console.WriteLine(s1.rollno);
+            Console.ReadKey();
+        }
+    }
+        
+    
+}
---
+++ AccessSpecifier/Assembly2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessSpecifier;
+namespace Assembly2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("RAUL", 55);
+            Console.WriteLine(p1.name);
+            Console.WriteLine(p1.age);
+            Console.ReadKey();
+        }
+    }
+}
